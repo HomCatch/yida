51d91023cdcedc3b2a9c456c0947ee1122c4ad2f
HomCatch <2424962073@qq.com>
2019-04-28 17:35:10 +0800

    第一次提交

--- miniprogram-service/src/main/java/com/xiaohe/etar/miniprogram/common/LogLogin.java
+++
@@ -1,9 +0,0 @@
-package com.xiaohe.etar.miniprogram.common;
-
-import java.lang.annotation.*;
-
-@Target(ElementType.METHOD)
-@Retention(RetentionPolicy.RUNTIME)
-@Documented
-public @interface LogLogin {
-}
--- mng-service/src/main/java/com/etar/purifier/common/annotation/LogLogin.java
+++
@@ -1,9 +0,0 @@
-package com.etar.purifier.common.annotation;
-
-import java.lang.annotation.*;
-
-@Target(ElementType.METHOD)
-@Retention(RetentionPolicy.RUNTIME)
-@Documented
-public @interface LogLogin {
-}
